06dba890672ca1e394d0af5c8e81100b996d57fb
Administrator <Administrator@FA2C5F27F8FB552>
2023-07-11 11:00:34 +0530

    Add project files.

---
+++ WebAPI_CRUD/Controllers/FriendsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendsController : ControllerBase
+    {
+        static List<string> myFriends = new List<string> () { "Vikram", "Ranjan", "Mano", "Vipul", "Rasheed","Marsh","Michael" };
+
+        #region Get
+        [HttpGet]
+        [Route("/List")]
+        public IActionResult GetAllFriends ()
+        {
+            return Ok(myFriends);
+        }
+        [HttpGet]
+        [Route("/Index/{id}")]
+        public IActionResult GetFriendByIndex (int id)
+        {
+            if (myFriends.Count < id)
+            {
+                return NotFound(string.Format($"Sorry friend not found with the id:" +id));
+            }
+            string friend = myFriends[id];
+            return Ok(friend);
+        }
+        [HttpGet]
+        [Route("/Character/{chars}")]
+        public  IActionResult GetFriendByChar(string chars)
+        {
+            var friend = from f in myFriends where f.StartsWith(chars) select f;
+            return Ok(friend);
+        }
+        [HttpGet]
+        [Route("/Count")]
+        public IActionResult GetCount() {
+            return Ok(myFriends.Count);
+        }
+        #endregion
+
+        #region Post
+        [HttpPost]
+        [Route("/add")]
+        public IActionResult AddFriend (string name)
+        {
+            myFriends.Add(name);
+            return Ok(name + " Is added to the list");
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete]
+        [Route("/delete/{id}")]
+        public IActionResult DeleteFriend (int id) {
+            string name = myFriends[id];
+            myFriends.RemoveAt(id);
+            return Accepted(name + " has been removed from Friend List.");
+        
+        }
+        [HttpDelete]
+        [Route("/delete/ByName/{name}")]
+        public IActionResult DeleteFriendByName(string name)
+        {
+            myFriends.Remove(name);
+            return Accepted(name + " has been removed from Friend List.");
+
+        }
+        #endregion
+
+        #region Put
+        [HttpPut]
+        [Route("/update/{id}/{name}")]
+        public IActionResult UpdateFriend (int id, string name)
+        {
+            string oldname = myFriends[id];
+            myFriends[id] = name;
+            return Ok(name + " Has been updated from " +oldname);
+        }
+        #endregion
+
+    }
+}
---
+++ WebAPI_CRUD/Controllers/ProductsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_CRUD.Controllers
+{
+    [Route("api/[Products]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        static List<string> myProducts = new List<string>()
+        { "Ac", "Washing Machine", "Fridge", "TV", "Fan", "Mobile", "Laptop" };
+
+        #region Get
+        [HttpGet]
+        [Route("/product/list")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(myProducts);
+        }
+        [HttpGet]
+        [Route("/Index/{id}")]
+        public IActionResult GetProductByIndex(int id)
+        {
+            if (myProducts.Count < id)
+            {
+                return NotFound(string.Format($"Sorry Product not found with the id:" + id));
+            }
+            string Product= myProducts[id];
+            return Ok(Product);
+        }
+        [HttpGet]
+        [Route("/Character/{chars}")]
+        public IActionResult GetProductByChar(string chars)
+        {
+            if (!myProducts.Contains(chars))
+            {
+                return NotFound(string.Format($"Sorry Product not found with the name:" + chars));
+            }
+            var Product= from f in myProducts where f.StartsWith(chars) select f;
+            return Ok(Product);
+        }
+        [HttpGet]
+        [Route("/Count")]
+        public IActionResult GetCount()
+        {
+            return Ok(myProducts.Count);
+        }
+        #endregion
+
+        #region Post
+        [HttpPost]
+        [Route("/add")]
+        public IActionResult AddProduct(string name)
+        {
+            myProducts.Add(name);
+            return Ok(name + " Is added to the list");
+            //return Accepted("/List", name + " Is added to the list");
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete]
+        [Route("/delete/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            string name = myProducts[id];
+            myProducts.RemoveAt(id);
+            return Accepted(name + " has been removed from ProductList.");
+
+        }
+        [HttpDelete]
+        [Route("/delete/ByName/{name}")]
+        public IActionResult DeleteProductByName(string name)
+        {
+            myProducts.Remove(name);
+            return Accepted(name + " has been removed from ProductList.");
+
+        }
+        #endregion
+
+        #region Put
+        [HttpPut]
+        [Route("/update/{id}/{name}")]
+        public IActionResult UpdateProduct(int id, string name)
+        {
+            string oldname = myProducts[id];
+            myProducts[id] = name;
+            return Ok(name + " Has been updated from " + oldname);
+        }
+        #endregion
+    }
+}
